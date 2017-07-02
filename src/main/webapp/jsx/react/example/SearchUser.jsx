f66ae1646936746436a60fdad19caab064611009
jinie(박진희) <jinie@coupang.com>
2017-07-03 02:36:12 +0900

    [Front] react 리스트 예제

---
+++ src/main/webapp/jsx/react/example/SearchUser.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export default class extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+        this.state = {
+            value: ''
+        };
+    }
+
+    handleChange(e) {
+        this.setState({
+            value: e.target.value
+        });
+    }
+
+    handleSubmit(e) {
+        if(e.key === "Enter") {
+            if(this.state.value.length === 0) {
+                return;
+            }
+
+            this.props.addToUsers(this.state.value);
+
+            this.setState({
+                value: ''
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <input value={this.state.value}
+                       onChange={this.handleChange}
+                       onKeyPress={this.handleSubmit} />
+            </div>
+        );
+    }
+}
